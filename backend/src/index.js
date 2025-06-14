f8fccf004857a022459a1183f00094cf9c264296
Saar16 <97218268+DharmaWarrior@users.noreply.github.com>
2025-06-14 13:43:04 +0530

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,10 @@ app.locals.io = io;
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 app.use(express.json({ type: 'application/json' }));
 
 // Connect to MongoDB
