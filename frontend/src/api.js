976b38d5fa2b32a63aef289c441b25ecbd19865b
Saar16 <97218268+DharmaWarrior@users.noreply.github.com>
2025-06-14 14:36:35 +0530

    updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,13 +5,15 @@ const API_URL = import.meta.env.VITE_API_URL + '/api';
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true, // if you use cookies/auth
+  // withCredentials: true, // if you use cookies/auth
 });
 
 // Add this interceptor:
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+  console.log("Hai token", token);
   if (token) {
+    console.log("Hai token idhar bhi");
     config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
